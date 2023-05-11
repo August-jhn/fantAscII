0ed75ebdf18109d903f01fe4ad73d57eaca0bfb0
Benjamin Weber <89813411+WeberMBenjamin@users.noreply.github.com>
2023-05-10 17:02:37 -0700

    Added Comment Documentation

--- edgeDetection.js
+++ edgeDetection.js
@@ -1,31 +1,28 @@
-// set a gradient for the char ascii art
-//var charGrad0 = " .'`^,:;I!i<>~+_-?[]{}1()|/tfjrxnuvczXYUJCLQ0)Zmwqpdbkhao*#MW&8%B@$"; 
-//var gradLen0 = charGrad0.length;
+//FantAscII v0.1 Copyright Ostler et al. (Jimmy Ostler, Pachie Ackerman, August Bergquist, Benjamin Weber)
+
+// set of gradients for default the char ascii art
 var charGrad0 = " .:-=+*#%@";
 var gradLen0 = charGrad0.length;
 var charGrad1 = "  .,:-=&";
 var gradLen1 = charGrad1.length;
 var charGrad2 = " -=#8";
 var gradLen2 = charGrad2.length;
-
-
+// list of all the gradients that can be looped through. New gradients go here
 var gradients = [charGrad0, charGrad1, charGrad2];
 var gradLens = [gradLen0, gradLen1, gradLen2];
-
-var gradVal = 0;
-var wantCharAscii = true;
-var darkMode = true;
-var cameraToggle = true;
-
+// Toggle Starting States
+var gradVal = 0;// Which gradient being used
+var wantCharAscii = true;// true is char while false is line
+var darkMode = true;// changes between light and dark mode
+var cameraToggle = true;// whether you are on video or image conversion. Start video and convert image call this
 // Set of pixalting width and height
 var pxlWidth = cameraWidth/(10/scalar);
 var pxlHeight = cameraHeight/(10/scalar);
 var imgWidth = uploadWidth/(10/scalars[scalarIndex])
 var imgHeight = uploadHeight/(10/scalars[scalarIndex])
-
 // Set a variable for the video tag
 const video = document.getElementById("video");
-var playing = false;
+var playing = false;// false when off or paused turns to true when video started or the play button is clicked
 
 //Function that copies to text that is currently in the image
 const copyToClipboardAsync = str => {
@@ -52,7 +49,6 @@ async function streamVideo() {
     
     try {
         // Get the video stream
-        
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
         window.stream = stream;
         video.srcObject = stream;
@@ -63,8 +59,8 @@ async function streamVideo() {
     }   
 }
 
+//Function to save the current canvas to a txt file. Essentially the copy button but putting it into a file
 function saveToTXT(){
-    
 //since the text content of the  paragraph includes <br> tags et cetera, it's better to recreate a massive string to be the content of the txt file
     let elt = document.createElement('a');
     elt.setAttribute('href', 'data:text/plane;charset=utf-8,' + encodeURIComponent(text));
@@ -140,9 +136,11 @@ function lineToArray(canvas){
     var array = imgData.data;
     var pxlImg = []
     for (let j=0; j<4*(cameraHeight)-1; j+=4){
+	// Have to loop through every fourth because getImageData returns a 1d array of the 3d image data
         var pxlRow = [];
         for (let i=(j*cameraWidth); i<(j*cameraWidth)+4*cameraWidth; i+=4){
-                var pix = array[i];
+                // Same reason that this has to loop through every fourth
+		var pix = array[i];
                 pxlRow.push(pix);
             }
         pxlImg.push(pxlRow);
@@ -163,13 +161,13 @@ function edgeDetection() {
             cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
             cv.Canny(src, src, 60, 100, 3, false);
             cv.imshow("edgeDetectionCanvas", src);
-            var input = lineToArray("edgeDetectionCanvas");
-            var binaryArray = arrayToBinaryArray(input, 0);
-            var chunks = arrayToChunks(binaryArray, chunksVal);
-            var lst = chunksToAscii(chunks);
-            var array = reshape(lst, ROW_LENGTH);
+            var input = lineToArray("edgeDetectionCanvas");// Turn the edge detection to an array
+            var binaryArray = arrayToBinaryArray(input, 0);// Change it to a 1 or 0 in array
+            var chunks = arrayToChunks(binaryArray, chunksVal);// Chunk the array
+            var lst = chunksToAscii(chunks);// Choose the Ascii character
+            var array = reshape(lst, ROW_LENGTH);// Return the array
             var lineArray = to_chrs(array);
-            displayArray('charCanvas', lineArray);
+            displayArray('charCanvas', lineArray);// Displays to the Canvas
             src.delete(); 
         }}
     }, 42);
@@ -209,9 +207,11 @@ function asciiConvert(canvas){
     var array = imgData.data;
     var pxlImg = []
     for (let j=0; j<4*(pxlHeight)-1; j+=4){
-        var pxlRow = [];
+        // Have to loop through every fourth because getImageData returns a 1d array of the 3d image data
+	var pxlRow = [];
         for (let i=(j*pxlWidth); i<(j*pxlWidth)+4*pxlWidth; i+=4){
-                var pix = array[i];
+                // Same reason that this has to loop through every fourth
+		var pix = array[i];
                 let convertNum = Math.trunc((pix*gradLens[gradVal])/255)%gradLens[gradVal];
                 var converted = gradients[gradVal][convertNum];
                 pxlRow.push(converted);
@@ -228,11 +228,11 @@ function charAscii(){
         if (wantCharAscii === true){
             drawCanvas();
             var src = cv.imread("streamCanvas");
-            cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
+            cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);// Grayscale
             let dsize = new cv.Size(pxlWidth,pxlHeight);
-            cv.resize(src, src, dsize, 0, 0, cv.INTER_CUBIC);
-            cv.imshow("grayCharStreamCanvas" ,src);
-            asciiConvert("grayCharStreamCanvas");
+            cv.resize(src, src, dsize, 0, 0, cv.INTER_CUBIC);// Resize to pixelate the image
+            cv.imshow("grayCharStreamCanvas" ,src);// Put the converted image onto a canvas to be accessed
+            asciiConvert("grayCharStreamCanvas");// Convert the array to Ascii
             src.delete(); 
         }}
     }, 42);
@@ -272,13 +272,14 @@ function addGrad(){
     }
 }
 
-//Resets the array with all the gradients and the array with the lengths to the default
+//Resets the array with all the gradients and the array with the lengths to the default when loaded in
 function resetGrad(){
     gradients = [charGrad0, charGrad1, charGrad2];
     gradLens = [gradLen0, gradLen1, gradLen2];
     gradVal = 0;
 }
 
+//Clear the canvas and toggles whether you want character or line ascii
 function swapAscii(){
     var asciiCanvas = document.getElementById('charCanvas');
     var context = asciiCanvas.getContext('2d');
@@ -317,7 +318,7 @@ function lightDarkMode(){
     }
 }
 
-
+//Same as the video converters but only called once when the button is clicked
 function convertImage(){
     if (document.getElementById("fileInput").files[0]==undefined){
         alert("No Image Uploaded");
